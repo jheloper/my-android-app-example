cee935b807f771ad3bccb49a2f4d0c7ca9b6ea87
Joonghyeon Kim <jheloper9102@gmail.com>
2018-05-14 07:20:01 +0900

    #2 메인액티비티 클래스에 서브액티비티로 이동하는 버튼 추가.

--- app/src/main/java/com/example/jhkim/myapplication/MainActivity.java
+++ app/src/main/java/com/example/jhkim/myapplication/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.jhkim.myapplication;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void mOnClick(View view) {
+        Intent intent = new Intent(this, SubActivity.class);
+        startActivity(intent);
     }
 }
